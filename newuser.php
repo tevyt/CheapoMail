3147a9451f1299750bf6b48c4d127a9743bf6cb3
Travis Smith <travissmith94@hotmail.com>
2014-12-05 15:08:18 -0500

    Form now redirects to home page

--- newuser.php
+++ newuser.php
@@ -12,6 +12,8 @@ $insertQuery = "INSERT INTO User (FirstName , LastName,
 Password , Username) VALUES (:FirstName,:LastName,:Password,:Username)";
 $query = $connection->prepare($insertQuery);
 $query->execute(array(':FirstName'=>$firstName, ':LastName'=>$lastName, ':Password'=>$password,':Username'=>$username));
+header("Location: homescreen.html");
+exit;
 ?>
 
  
