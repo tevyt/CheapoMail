a78d6cda538acaf6f18f5c03523729d8239b1122
Justen Morgan <justenamorgan@gmail.com>
2014-12-12 17:05:54 -0500

    Cleaned up database entries

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Group Members:
 Travis Smith, 620065811 (tevyt),
 Jerene Ricketts, 620067270 (tanjerene),
 Justen Morgan, 620070138 (boshes, previously alyssialui)
+
+Sample Login Credentials:
+
+Username: user
+Password: Password1
--- cheapomail.sql
+++ cheapomail.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2014 at 02:52 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Dec 12, 2014 at 11:03 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,31 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `message` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+`id` int(255) NOT NULL,
   `body` text NOT NULL,
   `subject` varchar(500) NOT NULL,
   `user_id` int(255) NOT NULL,
-  `recipient_ids` int(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`,`recipient_ids`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `recipient_ids` int(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `message`
 --
 
 INSERT INTO `message` (`id`, `body`, `subject`, `user_id`, `recipient_ids`) VALUES
-(1, 'I am doing web dev!', 'Web Dev', 4, 0),
-(2, 'I am doing web dev!', 'Web Dev', 4, 0),
-(3, 'I am doing web dev!', 'Web Dev', 4, 4),
-(4, 'I am doing web dev!', 'Web Dev', 4, 4),
-(5, 'I am doing web dev!', 'Web Dev', 4, 4),
-(6, 'It''s happening!!!!', 'IEEEXtreeme', 5, 4),
-(7, '', '', 4, 0),
-(8, 'How ya doing?', 'Hello World!', 4, 4),
-(9, 'Hai!', 'moo', 2, 2),
-(10, 'Hello World! :D', 'Hello', 6, 6),
-(11, 'cool!', 'Drop Down menu', 6, 6);
+(12, 'Welcome to Cheapomail!', 'Welcome!', 7, 7);
 
 -- --------------------------------------------------------
 
@@ -60,21 +48,18 @@ INSERT INTO `message` (`id`, `body`, `subject`, `user_id`, `recipient_ids`) VALU
 --
 
 CREATE TABLE IF NOT EXISTS `message_read` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   `reader_id` int(11) NOT NULL,
-  `date` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `message_id` (`message_id`,`reader_id`,`date`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+  `date` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `message_read`
 --
 
 INSERT INTO `message_read` (`id`, `message_id`, `reader_id`, `date`) VALUES
-(35, 10, 6, '12/12/2014'),
-(34, 11, 6, '12/12/2014');
+(36, 12, 7, '12/12/2014');
 
 -- --------------------------------------------------------
 
@@ -83,26 +68,61 @@ INSERT INTO `message_read` (`id`, `message_id`, `reader_id`, `date`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(255) unsigned NOT NULL,
   `FirstName` varchar(255) NOT NULL,
   `LastName` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `UserName` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `UserName` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `FirstName`, `LastName`, `Password`, `UserName`) VALUES
-(1, 'Travis', 'Smith', 'datatraveler', 'tevyt'),
-(2, 'Justen', 'Morgan', 'PooPooPoo1', 'moo'),
-(3, 'First', 'Second', 'Password1', 'user'),
-(4, 'Travis', 'Smith', 'Lifestartsn0w', 'tyrique'),
-(5, 'Dane', 'Miller', 'Drell1mal', 'drell1mal'),
-(6, 'Jerene', 'Ricketts', 'Ph1ll#!$', 'tanjerene');
+(7, 'First', 'Last', 'Password1', 'user');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+ ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`,`recipient_ids`);
+
+--
+-- Indexes for table `message_read`
+--
+ALTER TABLE `message_read`
+ ADD PRIMARY KEY (`id`), ADD KEY `message_id` (`message_id`,`reader_id`,`date`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `message_read`
+--
+ALTER TABLE `message_read`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(255) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
